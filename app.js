d87c0900271e4e6ef369dff710ffc5e08c740986
shehand <sonodewcomplex@gmail.com>
2019-02-17 11:26:04 +0530

    add user template

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var mongoose = require('mongoose');
 
 var app = express();
 
+mongoose.connect('mongodb://localhost:27017/medi-core');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
